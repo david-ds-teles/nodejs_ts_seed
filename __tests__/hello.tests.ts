862c3ba7b4633dc0aadbb3f69042346c46d79d91
David Teles <davidarius2@gmail.com>
2022-01-10 11:41:37 -0300

    configuring husk hooks, lint-stage

--- __tests__/hello.tests.ts
+++ __tests__/hello.tests.ts
@@ -1,6 +1,6 @@
-const message: string = 'hello ts-jest is fine';
-
 describe('testing jest configuration to work with typescript', () => {
+	const message: string = 'hello ts-jest is fine';
+
 	test('hello test config ok', () => {
 		expect(message).toBe('hello ts-jest is fine');
 	});
