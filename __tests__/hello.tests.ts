e030ddd2192c4f9bf5601e24cdc930a8e33f5566
David S Teles <david.ds.teles@gmail.com>
2022-01-13 11:40:51 -0300

    removed deprecated tslint and added and configured eslint

--- __tests__/hello.tests.ts
+++ __tests__/hello.tests.ts
@@ -1,5 +1,5 @@
 describe('testing jest configuration to work with typescript', () => {
-	const message: string = 'hello ts-jest is fine';
+	const message = 'hello ts-jest is fine';
 
 	test('hello test config ok', () => {
 		expect(message).toBe('hello ts-jest is fine');
