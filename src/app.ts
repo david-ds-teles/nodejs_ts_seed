b7565b96c44821a277cfc093dba55cd08c75b96d
David S Teles <94180051+david-ds-teles@users.noreply.github.com>
2022-01-12 21:53:24 -0300

    i18n configured and working with express (#3)

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
 import express, { Request, Response } from 'express';
+import { i18n } from './i18n';
 
 const app = express();
 const PORT = process.env.PORT;
 
+// middlewares
 app.use(express.json());
+app.use(i18n.init);
 
-app.use('/', (req: Request, rsp: Response) => {
-	rsp.send('hello from NodeJS Seed Project');
+//endpoints
+app.get('/', (req: Request, rsp: Response) => {
+	rsp.send(rsp.__('hello'));
+});
+
+app.get('/user/:user', (req: Request, rsp: Response) => {
+	const user = req.params['user'];
+	rsp.send(rsp.__('hello {{name}}', { name: user }));
+});
+
+app.use('/plural', (req: Request, rsp: Response) => {
+	rsp.send(rsp.__n('messages', 3));
 });
 
 app.listen(PORT, () => {
