0165dc2e11a3e8c48632456410678371c57379ab
David S Teles <david.ds.teles@gmail.com>
2022-01-12 21:50:33 -0300

    i18n configured and working with express

---
+++ src/i18n.ts
@@ -0,0 +1,19 @@
+/**
+ * l18n configuration to provide internationalized messages. This way, messages are easy to maintaining.
+ *
+ * The messages are generated inside locales directory, each json containing messages from the respective
+ * language.
+ */
+import { I18n } from 'i18n';
+import path from 'path';
+
+export const i18n = new I18n();
+
+i18n.configure({
+	defaultLocale: 'en',
+	directory: path.join('./', 'locales'),
+});
+
+i18n.setLocale('en');
+
+// console.log(i18n.__n('you have %s messages', 3));
